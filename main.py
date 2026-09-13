a6d5d55a104ad6cb53bfecaf7558f32c97ec092e
Kevin Jordan <kevinajt@gmail.com>
2026-09-13 08:15:58 -0400

    Refactor set_board(), request_input(). Refactor detect_result() into detect_win() and detect_draw(). Start refactor print_board()

--- main.py
+++ main.py
@@ -14,168 +14,84 @@ def switch_players():
 	else:
 		current_player = playerO
 
-def print_board():
-	print(top_line)
-	print(separator)
-	print(mid_line)
-	print(separator)
-	print(bot_line)
-
 def set_board():
-	global topl
-	global topm
-	global topr
-	global midl
-	global midm
-	global midr
-	global botl
-	global botm
-	global botr
+	global board
+	board = []
+
+	i = 0
+	while i < 9:
+		board.append(" ")
+		i += 1
+
+	# refactor lines - could be looped
 	global top_line
 	global mid_line
 	global bot_line
 	global separator
 
-	topl = " "
-	topm = " "
-	topr = " "
-	midl = " "
-	midm = " "
-	midr = " "
-	botl = " "
-	botm = " "
-	botr = " "
+	top_line = f" {board[0]} |  {board[1]}  | {board[2]} "
+	mid_line = f" {board[3]} |  {board[4]}  | {board[5]} "
+	bot_line = f" {board[6]} |  {board[7]}  | {board[8]} "
 
-	top_line = f" {topl} |  {topm}  | {topr} "
-	mid_line = f" {midl} |  {midm}  | {midr} "
-	bot_line = f" {botl} |  {botm}  | {botr} "
-	separator= "---|-----|---"
+def print_board():
+	separator = "---|-----|---"
+
+	i = 0
+	while i < 7:
+		print(f" {board[i]} |  {board[i+1]}  | {board[i+2]} ")
+		if i < 4:
+			print(separator)
+		i += 3
+
+	"""
+	print(top_line)
+	print(separator)
+	print(mid_line)
+	print(separator)
+	print(bot_line)
+	"""
 
 def set_options():
-
-	global option1
-	global option2
-	global option3
-	global option4
-	global option5
-	global option6
-	global option7
-	global option8
-	global option9
-
-	option1 = "1."
-	option2 = "2."
-	option3 = "3."
-	option4 = "4."
-	option5 = "5."
-	option6 = "6."
-	option7 = "7."
-	option8 = "8."
-	option9 = "9."
+	global option
+	option = []
+	i = 1
+	while i < 10:
+		option.append(f"{i}.")
+		i += 1
 
 def request_input():
 
-	global topl
-	global topm
-	global topr
-	global midl
-	global midm
-	global midr
-	global botl
-	global botm
-	global botr
+	global board
+	
 	global top_line
 	global mid_line
-	global bot_line
+	global bot_line 
 
-	global option1
-	global option2
-	global option3
-	global option4
-	global option5
-	global option6
-	global option7
-	global option8
-	global option9
+	global option
 
 	print(f"Current player: {current_player}")
 	print("Make selection:")
-	print(f" {option1}   {option2}   {option3}")
-	print(f" {option4}   {option5}   {option6}")
-	print(f" {option7}   {option8}   {option9}")
+	print(f" {option[0]}   {option[1]}   {option[2]}")
+	print(f" {option[3]}   {option[4]}   {option[5]}")
+	print(f" {option[6]}   {option[7]}   {option[8]}")
 
 	while True:
 		selection = input("Enter a number 1-9: ")
+		selection = int(selection) - 1
 
-		if selection == "1":
-			if (topl == playerX) or (topl == playerO):
+		if 0 <= selection <= 8:
+			if (board[selection] == playerX) or (board[selection] == playerO):
 				print("Invalid selection. Space is already occupied.")
 			else:
-				topl = current_player
-				option1 = "  "
-				break
-		elif selection == "2":
-			if (topm == playerX) or (topm == playerO):
-				print("Invalid selection. Space is already occupied.")
-			else:
-				topm = current_player
-				option2 = "  "
-				break
-		elif selection == "3":
-			if (topr == playerX) or (topr == playerO):
-				print("Invalid selection. Space is already occupied.")
-			else:
-				topr = current_player
-				option3 = "  "
-				break
-		elif selection == "4":
-			if (midl == playerX) or (midl == playerO):
-				print("Invalid selection. Space is already occupied.")
-			else:
-				midl = current_player
-				option4 = "  "
-				break
-		elif selection == "5":
-			if (midm == playerX) or (midm == playerO):
-				print("Invalid selection. Space is already occupied.")
-			else:
-				midm = current_player
-				option5 = "  "
-				break
-		elif selection == "6":
-			if (midr == playerX) or (midr == playerO):
-				print("Invalid selection. Space is already occupied.")
-			else:
-				midr = current_player
-				option6 = "  "
-				break
-		elif selection == "7":
-			if (botl == playerX) or (botl == playerO):
-				print("Invalid selection. Space is already occupied.")
-			else:
-				botl = current_player
-				option7 = "  "
-				break
-		elif selection == "8":
-			if (botm == playerX) or (botm == playerO):
-				print("Invalid selection. Space is already occupied.")
-			else:
-				botm = current_player
-				option8 = "  "
-				break
-		elif selection == "9":
-			if (botr == playerX) or (botr == playerO):
-				print("Invalid selection. Space is already occupied.")
-			else:
-				botr = current_player
-				option9 = "  "
+				board[selection] = current_player
+				option[selection] = "  "
 				break
 		else:
 			print("Invalid number. Please enter a number 1-9.")
 
-	top_line = f" {topl} |  {topm}  | {topr} "
-	mid_line = f" {midl} |  {midm}  | {midr} "
-	bot_line = f" {botl} |  {botm}  | {botr} "
+	top_line = f" {board[0]} |  {board[1]}  | {board[2]} "
+	mid_line = f" {board[3]} |  {board[4]}  | {board[5]} "
+	bot_line = f" {board[6]} |  {board[7]}  | {board[8]} "
 
 def show_scoreboard():
 	print("Current Score")
@@ -203,53 +119,45 @@ def play_again():
 		else:
 			print("Invalid selection. Please enter y to play again or n to exit.")
 
-def detect_result():
-	if (
-		# Horizontal checks
-		(topl == playerX and topm == playerX and topr == playerX) or 
-		(midl == playerX and midm == playerX and midr == playerX) or
-		(botl == playerX and botm == playerX and botr == playerX) or
-		# Vertical checks
-		(topl == playerX and midl == playerX and botl == playerX) or
-		(topm == playerX and midm == playerX and botm == playerX) or
-		(topr == playerX and midr == playerX and botr == playerX) or
-		# Diagonal checks
-		(topl == playerX and midm == playerX and botr == playerX) or
-		(topr == playerX and midm == playerX and botl == playerX) or
-		# Horizontal checks
-		(topl == playerO and topm == playerO and topr == playerO) or 
-		(midl == playerO and midm == playerO and midr == playerO) or
-		(botl == playerO and botm == playerO and botr == playerO) or
-		# Vertical checks
-		(topl == playerO and midl == playerO and botl == playerO) or
-		(topm == playerO and midm == playerO and botm == playerO) or
-		(topr == playerO and midr == playerO and botr == playerO) or
-		# Diagonal checks
-		(topl == playerO and midm == playerO and botr == playerO) or
-		(topr == playerO and midm == playerO and botl == playerO)
-	):
-		print_board()
-		print(f"{current_player} wins!")
-		update_scoreboard(current_player)
-		show_scoreboard()
-		play_again()
-	elif (
-		topl != " " and topm != " " and topr != " " and
-		midl != " " and midm != " " and midr != " " and
-		botl != " " and botm != " " and botr != " "
-	):
-		print_board()
-		print("Game drawn.")
-		play_again()
+def detect_win():
+	win_condition = [
+					# horizontal wins
+					[0,1,2],[3,4,5],[6,7,8],
+				  	# vertical wins
+				  	[0,3,6],[1,4,7],[2,5,8],
+					# diagonal wins
+					[0,4,8],[2,4,6]
+					]
+
+	for i in win_condition:
+		if (
+			board[i[0]] == current_player and
+			board[i[1]] == current_player and
+			board[i[2]] == current_player
+		):
+			print_board()
+			print(f"{current_player} wins!")
+			update_scoreboard(current_player)
+			show_scoreboard()
+			play_again()
+
+def detect_draw():
+	for i in board:
+			if i == " ":
+				return
+
+	print_board()
+	print("Game drawn.")
+	play_again()
 
 def main():
-
 	while True:
 		set_board()
 		set_options()
 
 		while True:
-			detect_result()
+			detect_win()
+			detect_draw()
 			switch_players()
 			print_board()
 			request_input()
